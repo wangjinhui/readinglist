578177e6d8fcf5f40a6155a66ae951793f83677d
sillyboy <wjh20136413>
2018-03-11 19:48:48 +0800

    pom依赖的讲解

--- src/main/java/com/manning/readinglist/ReadinglistApplication.java
+++ src/main/java/com/manning/readinglist/ReadinglistApplication.java
@@ -3,7 +3,8 @@ package com.manning.readinglist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication//开启了Spring的组件扫描和Spring Boot的自动配置功能
+//@Configuration,@EnableAutoConfiguration, @ComponentScan
 public class ReadinglistApplication {
 
 	public static void main(String[] args) {
